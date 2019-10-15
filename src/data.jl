514255d5859cbf2c16963bd601eb6b72fc00e0fa
anandijain <anandj@uchicago.edu>
2019-10-15 15:08:18 -0500

    read nasa insight twins csv

---
+++ src/data.jl
@@ -0,0 +1,12 @@
+# peering into InSight data
+
+using CSV, DataFrames
+function read_temp_wind_data()
+    # data found at https://atmos.nmsu.edu/data_and_services/atmospheres_data/INSIGHT/insight.html#Selecting_Data
+    # placed in parent dir/data
+    path = "../data/twins_bundle/data_calibrated/sol_0000_0122/"
+    x  = readdir(path)
+    fn = "twins_calibevent_0118_01.csv"
+    df = CSV.read(string(path, fn))
+    return df
+end
